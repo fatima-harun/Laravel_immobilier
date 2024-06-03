376b6aa03a44ea383a7f3a5c193be909adb05ed6
fatima-harun <faabintu@gmail.com>
2024-06-03 12:49:36 +0000

    creation de model et controller

---
+++ app/Http/Controllers/CommentaireController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CommentaireController extends Controller
+{
+    public function indexcommentaire(){
+        return view('index');
+    }
+}
